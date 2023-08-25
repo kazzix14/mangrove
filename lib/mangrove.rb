fad25f660670298d1b3e2d2678709aa2aa6b0744
Kazuma Murata <kazzix14@gmail.com>
2023-08-25 15:08:22 +0900

    to require modules

--- lib/mangrove.rb
+++ lib/mangrove.rb
@@ -2,7 +2,11 @@
 # frozen_string_literal: true
 
 require "sorbet-runtime"
+
 require_relative "mangrove/version"
+require_relative "mangrove/option"
+require_relative "mangrove/result"
+require_relative "mangrove/control_flow"
 
 # Mangrove
 module Mangrove; end
--- lib/mangrove/interfaces.rb
+++
@@ -1,8 +0,0 @@
-# typed: strict
-# frozen_string_literal: true
-
-module Mangrove
-  module Interfaces
-    require_relative "interfaces/control_signal"
-  end
-end
