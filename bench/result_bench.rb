cbdf135e5311c7b4e8dceca6616a47cd22e871f0
kazzix <kazzix14@gmail.com>
2024-06-04 19:46:18 +0900

    update

--- bench/result_bench.rb
+++ bench/result_bench.rb
@@ -3,40 +3,52 @@
 
 require "mangrove"
 
+# T::Configuration.default_checked_level = :never
+# error_handler = lambda do |error, *_|
+#   # Log error somewhere
+# end
+
+# Suppresses errors caused by T.cast, T.let, T.must, etc.
+# T::Configuration.inline_type_error_handler = error_handler
+# Suppresses errors caused by incorrect parameter ordering
+# T::Configuration.sig_validation_error_handler = error_handler
+
 require "sorbet-runtime"
 require "benchmark"
+require "vernier"
 
-COUNT = 1000000
+COUNT = 10000000
 
 Benchmark.bm(24) { |r|
+  Vernier.profile(out: "vernier_profile.json", interval: 19) {
+    r.report("Exception") {
+      COUNT.times do
+        begin
+          if rand(2).zero?
+            _ok = 1
+          else
+            raise StandardError, "err"
+          end => return_value
+        rescue StandardError => e
+          _err = e.message
+        end
+      end
+    }
 
-  r.report("Exception") {
-    COUNT.times do
-      begin
+    r.report("Mangrove::Result") {
+      COUNT.times do
         if rand(2).zero?
-          _ok = 1
+          T.cast(Mangrove::Result::Ok.new(1), Mangrove::Result[T.untyped, T.untyped])
         else
-          raise StandardError, "err"
+          T.cast(Mangrove::Result::Err.new("err"), Mangrove::Result[T.untyped, T.untyped])
         end => return_value
-      rescue StandardError => e
-        _err = e.message
-      end
-    end
-  }
 
-  r.report("Mangrove::Result") {
-    COUNT.times do
-      if rand(2).zero?
-        T.cast(Mangrove::Result::Ok.new(1), Mangrove::Result[T.untyped, T.untyped])
-      else
-        T.cast(Mangrove::Result::Err.new("err"), Mangrove::Result[T.untyped, T.untyped])
-      end => return_value
-
-      if return_value.is_a?(Mangrove::Result::Ok)
-        _ok = return_value.ok_inner
-      else
-        _err = return_value.err_inner
+        if return_value.is_a?(Mangrove::Result::Ok)
+          _ok = return_value.ok_inner
+        else
+          _err = return_value.err_inner
+        end
       end
-    end
+    }
   }
 }
